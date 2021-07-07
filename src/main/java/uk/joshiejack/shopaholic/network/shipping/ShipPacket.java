d41dc34e11162fd58ee263324811b523980bff9f
joshiejack <joshjackwildman@gmail.com>
2021-07-07 13:50:34 +0100

    Actually play the kerching sound.

--- src/main/java/uk/joshiejack/shopaholic/network/shipping/ShipPacket.java
+++ src/main/java/uk/joshiejack/shopaholic/network/shipping/ShipPacket.java
@@ -5,6 +5,7 @@ import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.Minecraft;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.SoundCategory;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -12,6 +13,7 @@ import net.minecraftforge.fml.network.NetworkDirection;
 import uk.joshiejack.penguinlib.network.packet.AbstractSyncCompoundNBTPacket;
 import uk.joshiejack.penguinlib.util.PenguinLoader;
 import uk.joshiejack.penguinlib.util.helpers.generic.StringHelper;
+import uk.joshiejack.shopaholic.Shopaholic;
 import uk.joshiejack.shopaholic.client.ShopaholicClientConfig;
 import uk.joshiejack.shopaholic.client.gui.DepartmentScreen;
 import uk.joshiejack.shopaholic.client.shipping.Shipped;
@@ -30,8 +32,11 @@ public class ShipPacket extends AbstractSyncCompoundNBTPacket {
     public void handle(PlayerEntity player) {
         Set<Shipping.SoldItem> toSell = Sets.newHashSet();
         Shipping.readHolderCollection(tag.getList("ToSell", 10), toSell);
-        if (ShopaholicClientConfig.enableShippingTicker.get())
+        if (ShopaholicClientConfig.enableShippingTicker.get()) {
+            player.level.playSound(player, player.xo, player.yo, player.zo, Shopaholic.ShopaholicSounds.KERCHING.get(), SoundCategory.PLAYERS, 1.5F, player.level.random.nextFloat() * 0.1F + 0.9F);
             MinecraftForge.EVENT_BUS.register(new TrackingRenderer(toSell));
+        }
+
         //Merge in the newly sold items to the sold list
         Set<Shipping.SoldItem> merged = Sets.newHashSet();
         for (Shipping.SoldItem holder: toSell) {
